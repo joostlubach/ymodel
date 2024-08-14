9859c7cd71d1f49523e387f330a6df6bd9534e50
Joost Lubach <joostlubach@gmail.com>
2024-08-14 12:53:20 +0200

    Type renmae.

--- src/types.ts
+++ src/types.ts
@@ -30,12 +30,12 @@ export interface ModelConstructor<M extends Model> {
   new (...args: any[]): M
 
   deserialize<M extends Model>(raw: ModelSerialized, ...context: {} extends Context ? [] : [context: Context]): M
-  serializePartial<M extends Model>(model: Partial<AttributesOf<M>>): ModelSerialized
+  serializePartial<M extends Model>(model: Partial<ModelAttributes<M>>): ModelSerialized
 }
 export type ModelSerialized = Record<string, any>
 
-export type AttributesOf<M extends Model> = Omit<{[K in keyof M as M[K] extends AnyFunction ? never : K]: M[K]}, 'serialized'>
-export type PrimitiveAttributesOf<M extends Model> = Omit<AttributesOf<M>, 'id' | 'createdAt' | 'updatedAt'>
+export type ModelAttributes<M extends Model> = Omit<{[K in keyof M as M[K] extends AnyFunction ? never : K]: M[K]}, '$serialized'>
+export type ModelData<M extends Model> = Omit<ModelAttributes<M>, 'id' | 'createdAt' | 'updatedAt'>
 
 export interface Context {}
 
