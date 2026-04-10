d2cc835f333b544137f99f5168ce784652e15f21
Joost Lubach <joostlubach@gmail.com>
2026-04-10 22:58:47 +0200

    Fix

--- src/propSerializer.ts
+++ src/propSerializer.ts
@@ -2,7 +2,7 @@ import { Constructor, EmptyObject } from 'ytil'
 import { propSerializers } from './registry'
 import { PropertySerializer } from './types'
 
-export function propSerializer<T, S, O = EmptyObject>(type: Constructor<T>, serializer: PropertySerializer<T, S, O>): void
+export function propSerializer<T extends object, S, O = EmptyObject>(type: Constructor<T>, serializer: PropertySerializer<T, S, O>): void
 export function propSerializer<T, S, O = EmptyObject>(type: Function, serializer: PropertySerializer<T, S, O>): void
 export function propSerializer<T, S, O = EmptyObject>(type: Function, serializer: PropertySerializer<T, S, O>) {
   propSerializers.set(type, serializer)
